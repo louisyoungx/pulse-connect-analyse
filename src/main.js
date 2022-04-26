7d447db85e5c3a4dc11d65dd1ed423d41b58b198
louisyoungx <1462648167@qq.com>
2022-04-26 20:00:53 +0800

    refactor: simplify dependency import

--- src/main.js
+++ src/main.js
@@ -4,20 +4,8 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vant from 'vant'
+import 'vant/lib/index.css';
 
+// Create App
 const PCA = createApp(App);
 PCA.use(store).use(router).use(vant).mount('#app')
-
-/* Element-Plus Component */
-import 'element-plus/lib/theme-chalk/index.css';
-import {
-    ElCard,
-    ElButton,
-    ElRow,
-    ElCarousel,
-    ElCarouselItem,
-    ElImage,
-    ElPageHeader,
-    ElDatePicker
-} from 'element-plus';
-PCA.use(ElCard).use(ElButton).use(ElRow).use(ElCarousel).use(ElCarouselItem).use(ElImage).use(ElPageHeader).use(ElDatePicker)
